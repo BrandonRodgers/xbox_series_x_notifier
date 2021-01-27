0c116332f60f33a8380ea1b46ec4099e86b87a81
Brandon Rodgers <brandonx87@gmail.com>
2021-01-27 11:18:47 -0600

    Removed some websites that don't play well with this automated script

--- program.py
+++ program.py
@@ -53,21 +53,9 @@ s.enter(sleep_time, priority, checkOnWebsite, (s,'https://www.bestbuy.com/site/c
 s.enter(sleep_time, priority, checkOnWebsite, (s,'https://www.bestbuy.com/site/combo/ps5-consoles/8f146095-0a5f-4993-b123-711a1d34745b',best_buy_website_name,'Extra Controller',best_buy_keyword,headers,))
 s.enter(sleep_time, priority, checkOnWebsite, (s,'https://www.bestbuy.com/site/combo/ps5-consoles/c471fae2-1d2c-4870-ad3d-d39bffa39af2',best_buy_website_name,'Spider Man, COD, Demon Souls',best_buy_keyword,headers,))
 
-amazon_website_name = "Amazon"
-amazon_keyword = "unavailable"
-s.enter(sleep_time, priority, checkOnWebsite, (s,'https://www.amazon.com/dp/B08FC5L3RG/',amazon_website_name,'',amazon_keyword,headers,))
-
 gamestop_website_name = "Game Stop"
 gamestop_keyword = "Not Available"
 s.enter(sleep_time, priority, checkOnWebsite, (s,'https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5/11108140.html?condition=New',gamestop_website_name,'',gamestop_keyword,headers,))
-
-playstation_website_name = "Playstation"
-playstation_keyword = "Out of Stock"
-s.enter(sleep_time, priority, checkOnWebsite, (s,'https://direct.playstation.com/en-us/consoles/console/playstation5-console.3005816',playstation_website_name,'',playstation_keyword,headers,))
-
-target_website_name = "Target"
-target_keyword = "Sold out"
-s.enter(sleep_time, priority, checkOnWebsite, (s,'https://www.target.com/p/playstation-5-console/-/A-81114595',target_website_name,'',target_keyword,headers,))
 
 walmart_website_name = "Walmart"
 wlmart_keyword = "out of stock"
