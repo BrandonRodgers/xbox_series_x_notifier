bb67ac452fbe17d9d1aaccf7baa3f505f8b7fd9b
Brandon Rodgers <brandonx87@gmail.com>
2021-06-18 14:55:33 -0500

    A few more updates to links

--- program.py
+++ program.py
@@ -84,6 +84,10 @@ costco_website_name = "Costco"
 costco_keyword = "Out of Stock"
 s.enter(sleep_time, priority, checkOnWebsite, (s,'https://www.costco.com/xbox-series-x-1tb-console-with-additional-controller.product.100691493.html',costco_website_name,'extra controller',costco_keyword,headers,))
 
+sams_website_name = "Sams"
+sams_keyword = "Out of stock"
+s.enter(sleep_time, priority, checkOnWebsite, (s,'https://www.samsclub.com/p/xbox-series-x-bundle-charger-wired-headset/prod25940011?xid=plp_product_14',sams_website_name,'charger, headphones',sams_keyword,headers,))
+
 ant_online_website_name = "Ant Online"
 ant_online_keyword = "Sold Out"
 # s.enter(sleep_time, priority, checkOnWebsite, (s,'https://www.antonline.com/Microsoft/Electronics/Gaming_Devices/Gaming_Consoles/1415507',ant_online_website_name,'',ant_online_keyword,headers,))
